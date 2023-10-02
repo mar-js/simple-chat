dbce1389eaa1e476bf6d2c078f774b18d0b13104
Martin <maguera37@gmail.com>
2023-10-01 23:03:00 -0300

    feat: add config of server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const PORT = process.env.PORT || 3000
+
+const APP = express()
+
+APP.get('/', (req, res) => {
+ res.send('<h1>Hello Word</h1>')
+})
+
+APP.list(PORT, () => {
+ console.log(`Server runing on port localhost:${PORT}`)
+})
