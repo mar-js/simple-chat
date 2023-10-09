cbae7d625424464a6c2fdf15a790de0152d0f4c9
Martin <maguera37@gmail.com>
2023-10-08 23:03:00 -0300

    feat: add config of websocket

--- server/index.js
+++ server/index.js
@@ -1,14 +1,26 @@
+import { createServer } from 'node:http'
 import express from 'express'
+import { Server } from 'socket.io'
 
 const PORT = process.env.PORT || 3000
 const PATH_ROOT_PROJECT = process.cwd()
 
 const APP = express()
+const SERVER = createServer(APP)
+const IO = new Server(SERVER)
+
+IO.on('connection', (socket) => {
+  console.log('A user has connected!')
+
+  socket.on('disconnect', () => {
+    console.log('An user has disconnected!')
+  })
+})
 
 APP.get('/', (req, res) => {
   res.sendFile(PATH_ROOT_PROJECT + '/client/index.html')
 })
 
-APP.listen(PORT, () => {
+SERVER.listen(PORT, () => {
   console.log(`Server runing on port localhost:${PORT}`)
 })
